88f0601d6bf9b3817cc2027e661c8cb4df54d8db
inkwrenallen <145377496+inkwrenallen@users.noreply.github.com>
2024-02-28 13:15:51 -0500

    finished problem one on step one of problem two

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompting for and getting a maximum number from the user.
+            Console.WriteLine("please enter number maximum:");
+            int maximum = int.Parse(Console.ReadLine());
+
+            //Print all the even numbers from 0 up to the maximum number 
+            //(or 1 less than the maximum number if the maximum number is odd).
+            for (int i = 0; i < maximum; i++)
+            {
+               if(i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
